b2f7912485ff35ad7192afbc717fedb013c379f5
liwen.li <lli15@kent.edu>
2023-01-13 11:30:04 +0800

    update

--- pages/拐点分析.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-import pandas as pd
-
-upload_data = st.file_uploader(label = '上传表格',
-                                accept_multiple_files=False)
-
-if upload_data is not None:
-    data_csv = pd.read_excel(upload_data)
-    st.write(data_csv)
